608e431e40c13834842a9b62089120dfc064c067
Wielton <grantwielgosz@gmail.com>
2022-04-08 21:55:41 -0600

    I think everything is working as needed to

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@ function userLogout(){
     Cookies.remove('sessionToken');
     window.location.href='/index.html';
 }
+// 
 function colorLoad(){
     axios.request({
         method: "GET",
@@ -16,8 +17,8 @@ function loadSuccess(response){
         Math.floor(Math.random() * colorsArray.length)
     ]
     console.log(color.name, color.year, color.color);
-    document.getElementById('colorName').innerText=color.name;
-    document.getElementById('yearCreated').innerText=color.year;
+    document.getElementById('colorName').innerText="This color is " +color.name;
+    document.getElementById('yearCreated').innerText="It was created in " +color.year;
     document.getElementById('colorBox').style.backgroundColor=color.color;
     /*
     colorsArray.forEach(color => {
@@ -32,16 +33,40 @@ function loadSuccess(response){
 function fail(error){
     console.log(error);
 }
+
+// On page load will check for an active cookie and if not active will redirect to login screen
 function checkCookie(){  
         if(document.cookie.length!=0){  
             console.log(document.cookie);  
         } else {  
-            window.location.href='/index.html'; 
-        }  
+            returnToLoginScreen();
+            }  
     } 
+    function returnToLoginScreen(){
+        // Get modal and display failed login message
+        
+        modal.style.display = "block";
+        var returnMessage = document.getElementById('returnToLogin');
+        returnMessage.innerText = "You are not logged in.  Click the button to return to login page";
+        var returnBtn = document.getElementById('returnToLoginBtn');
+        returnBtn.addEventListener('click', buttonReturn);
+    }
+    
+    // When the user clicks on <span> (x), the activity modal closes
+    function closeModal(){
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        
+        if (event.target == modal) {
+        modal.style.display = "none";
+        }
+    }
+function buttonReturn(){
+    window.location.href='/index.html';
+}
 
-
+var modal = document.getElementById("returnToLoginModal");
 document.getElementById('logoutBtn').addEventListener('click', userLogout);
 window.addEventListener('load', colorLoad);
-// window.addEventListener('load', cookieCheck);
-// 
